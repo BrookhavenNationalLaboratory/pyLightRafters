021fb446cafb86f9309cdadaf8c6c10386b58d44
Thomas A Caswell <tcaswell@bnl.gov>
2014-04-01 17:01:19 -0400

    ENH : added option to namedtmpfile to get more than one filename

--- pyRafters/tests/testing_helpers.py
+++ pyRafters/tests/testing_helpers.py
@@ -1,21 +1,25 @@
 from __future__ import (absolute_import, division, print_function,
                         unicode_literals)
 import six
+from six.moves import range
 import tempfile
 import os
 from functools import wraps
 
 
-def namedtmpfile(suffix):
+def namedtmpfile(suffix, n=1):
     def outer(fun):
         @wraps(fun)
         def inner():
-            tmp = tempfile.NamedTemporaryFile(delete=False, suffix=suffix)
-            fname = tmp.name
-            tmp.close()
+            fnames = []
+            for j in range(n):
+                tmp = tempfile.NamedTemporaryFile(delete=False, suffix=suffix)
+                fnames.append(tmp.name)
+                tmp.close()
             try:
-                fun(fname)
+                fun(*fnames)
             finally:
-                os.remove(fname)
+                for fname in fnames:
+                    os.remove(fname)
         return inner
     return outer
